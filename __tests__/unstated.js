ed949ab0a5dfada862f42a34073e79d0d95ad59a
Paul Phan <phanminh65@gmail.com>
2018-06-08 00:16:03 +0700

    update readme

--- __tests__/unstated.js
+++ __tests__/unstated.js
@@ -4,77 +4,77 @@ import renderer from 'react-test-renderer';
 import { Provider, Subscribe, Container } from '../src/unstated';
 
 function render(element) {
-	return renderer.create(element).toJSON();
+  return renderer.create(element).toJSON();
 }
 
 class CounterContainer extends Container<{ count: number }> {
-	state = { count: 0 };
-	increment(amount = 1) {
-		this.setState({ count: this.state.count + amount });
-	}
-	decrement(amount = 1) {
-		this.setState({ count: this.state.count - amount });
-	}
+  state = { count: 0 };
+  increment(amount = 1) {
+    this.setState({ count: this.state.count + amount });
+  }
+  decrement(amount = 1) {
+    this.setState({ count: this.state.count - amount });
+  }
 }
 
 class AmounterContainer extends Container<{ amount: number }> {
-	state = { amount: 1 };
-	setAmount(amount) {
-		this.setState({ amount });
-	}
+  state = { amount: 1 };
+  setAmount(amount) {
+    this.setState({ amount });
+  }
 }
 
 function Counter() {
-	return (
-		<Subscribe to={[CounterContainer]}>
-			{counter => (
-				<div>
-					<span>{counter.state.count}</span>
-					<button onClick={() => counter.decrement()}>-</button>
-					<button onClick={() => counter.increment()}>+</button>
-				</div>
-			)}
-		</Subscribe>
-	);
+  return (
+    <Subscribe to={[CounterContainer]}>
+      {counter => (
+        <div>
+          <span>{counter.state.count}</span>
+          <button onClick={() => counter.decrement()}>-</button>
+          <button onClick={() => counter.increment()}>+</button>
+        </div>
+      )}
+    </Subscribe>
+  );
 }
 
 function CounterWithAmount() {
-	return (
-		<Subscribe to={[CounterContainer, AmounterContainer]}>
-			{(counter, amounter) => (
-				<div>
-					<span>{counter.state.count}</span>
-					<button onClick={() => counter.decrement(amounter.state.amount)}>
-						-
-					</button>
-					<button onClick={() => counter.increment(amounter.state.amount)}>
-						+
-					</button>
-				</div>
-			)}
-		</Subscribe>
-	);
+  return (
+    <Subscribe to={[CounterContainer, AmounterContainer]}>
+      {(counter, amounter) => (
+        <div>
+          <span>{counter.state.count}</span>
+          <button onClick={() => counter.decrement(amounter.state.amount)}>
+            -
+          </button>
+          <button onClick={() => counter.increment(amounter.state.amount)}>
+            +
+          </button>
+        </div>
+      )}
+    </Subscribe>
+  );
 }
 
 function CounterWithAmountApp() {
-	return (
-		<Subscribe to={[AmounterContainer]}>
-			{amounter => (
-				<div>
-					<Counter />
-					<input
-						type="number"
-						value={amounter.state.amount}
-						onChange={event => {
-							amounter.setAmount(parseInt(event.currentTarget.value, 10));
-						}}
-					/>
-				</div>
-			)}
-		</Subscribe>
-	);
+  return (
+    <Subscribe to={[AmounterContainer]}>
+      {amounter => (
+        <div>
+          <Counter />
+          <input
+            type="number"
+            value={amounter.state.amount}
+            onChange={event => {
+              amounter.setAmount(parseInt(event.currentTarget.value, 10));
+            }}
+          />
+        </div>
+      )}
+    </Subscribe>
+  );
 }
 
 test('basic', () => {
-	// still too lazy
+  // still too lazy
 });
